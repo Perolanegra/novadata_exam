9a072d6b7dcd533c9ca906c0f4115ddb03417ca0
perolanegra <pedratto3@gmail.com>
2024-01-23 19:44:42 -0300

    remove unused code

--- src/controllers/PostsCtrl.js
+++ src/controllers/PostsCtrl.js
@@ -7,25 +7,13 @@ module.exports = {
     // requisição que insere o post.
     const postData = req.body;
     try {
-      // await Posts.find(postData, (e, resp) => {
-      //     if(e) {
-      //         return res.status(400).send({ err: { message: 'Operação Indisponível no momento.', e } });
-      //     }
-      //     hasData = resp.length ? true : false;
-      // });
-      // if(hasData) {
-      //     return res.status(409).send({ err: { message: 'Estabelecimento já cadastrado.' } });
-      // }
-
       const posts = await Posts.create(postData);
       return res.send(posts);
     } catch (e) {
-      return res
-        .status(500)
-        .send({
-          friendly: "Não foi possível adicionar o post.",
-          err: { message: e.message, name: e.name },
-        });
+      return res.status(500).send({
+        friendly: "Não foi possível adicionar o post.",
+        err: { message: e.message, name: e.name },
+      });
     }
   },
 
@@ -34,11 +22,9 @@ module.exports = {
       const postsArr = await Posts.find();
       return res.send(postsArr);
     } catch (e) {
-      return res
-        .status(400)
-        .send({
-          err: { message: "Não foi possível retornar as Postagens.", e },
-        });
+      return res.status(400).send({
+        err: { message: "Não foi possível retornar as Postagens.", e },
+      });
     }
   },
 };
